14d40f43b92db1d38d30988ec1974f2cdab47bc2
JerowinGeo <147139744+JerowinGeo@users.noreply.github.com>
2024-09-30 16:02:56 +0530

    Update Image transformations[1-3].py

--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -3,39 +3,52 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-input_img=cv2.imread("color image of flower.jpg")
-input_img=cv2.cvtColor(input_img,cv2.)
+
+input_image=cv2.imread("vijay.png")
+input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
 plt.axis('off')
-plt.imshow(input_img)
+plt.imshow(input_image)
 plt.show()
-rows,cols,dim=input_img.shape
-M=np.float32([[1,0,20],
-             [0,1,50],
-             [0,0,1]])
-translated_img=cv2.
+rows,cols,dim=input_image.shape
+M=np.float32([[1,0,50],  [0,1,100],  [0,0,1]])
+translated_image=cv2.warpPerspective(input_image,M,(cols,rows))
 plt.axis('off')
+plt.imshow(translated_image)
+plt.show()
 
 
 
 ##ii)Image Scaling
 
-scaled_img=cv2.warpPerspective(input_img,M,(cols,rows))
-plt.axis('off')
-plt.imshow(scaled_img)
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+org_image = cv2.imread("vijay.png")
+org_image = cv2.cvtColor(org_image,cv2.COLOR_BGR2RGB)
+plt.imshow(org_image)
 plt.show()
-
+rows,cols,dim = org_image.shape
+M = np.float32([[1.5,0,0],[0,1.7,0],[0,0,1]])
+scaled_img = cv2.warpPerspective(org_image,M,(cols*2,rows*2))
+plt.imshow(org_image)
+plt.show()
 
 ##iii)Image Shearing
-M_x=np.float32([[1,0.2,0],
-               [0,1,0],
-               [0,0,1]])
-M_y=np.
-sheared_img_xaxis=cv2.warpPerspective(input_img,M_x,(cols,rows))
-sheared_img_yaxis=
-plt.axis('off')
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+org_image = cv2.imread("vijay.png")
+org_image = cv2.cvtColor(org_image,cv2.COLOR_BGR2RGB)
+plt.imshow(org_image)
+plt.show()
+rows,cols,dim = org_image.shape
+M_X = np.float32([[1,0.5,0],[0,1,0],[0,0,1]])
+M_Y = np.float32([[1,0,0],[0.5,1,0],[0,0,1]])
+sheared_img_xaxis = cv2.warpPerspective(org_image,M_X,(int(cols*1.5),int(rows*1.5)))
+sheared_img_yaxis = cv2.warpPerspective(org_image,M_Y,(int(cols*1.5),int(rows*1.5)))
 plt.imshow(sheared_img_xaxis)
 plt.show()
-plt.axis
+plt.imshow(sheared_img_yaxis)
 plt.show()
 
 
